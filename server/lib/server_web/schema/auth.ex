694e00af7fe77f6847d848d4bfc38bdec5c1353e
abehidek <hidek.abe@outlook.com>
2023-06-06 21:15:43 -0300

    feat(server): me query and signOut mutation gql endpoints

--- server/lib/server_web/schema/auth.ex
+++ server/lib/server_web/schema/auth.ex
@@ -22,12 +22,6 @@ defmodule ServerWeb.Schema.Auth do
     field(:password, non_null(:string))
   end
 
-  object :auth_query do
-    field :fizz, :string do
-      resolve(fn _, _ -> {:ok, "Fizz buzz"} end)
-    end
-  end
-
   input_object :sign_in_input do
     field(:email, non_null(:string))
     field(:password, non_null(:string))
@@ -73,6 +67,63 @@ defmodule ServerWeb.Schema.Auth do
 
           {:error, %Ecto.Changeset{} = changeset} ->
             {:error, Error.error_payload(changeset)}
+        end
+      end)
+    end
+
+    field :sign_out, type: non_null(:me_payload) do
+      arg(:auth, non_null(:me_input))
+
+      resolve(fn %{auth: %{token: token}}, _ ->
+        IO.inspect(token)
+
+        case Server.Auth.get_user_session_by_token(token) do
+          %Server.Auth.UserSession{} = user_session ->
+            case user_session |> Server.Auth.delete_user_session() do
+              {:ok, %Server.Auth.UserSession{} = deleted_user_session} ->
+                {:ok, deleted_user_session |> Server.Repo.preload(:user)}
+
+              {:error, %Ecto.Changeset{} = changeset} ->
+                {:error, Error.error_payload(changeset)}
+
+              _ ->
+                {:error, "Internal Server Error"}
+            end
+
+          nil ->
+            {:error, "Session not found"}
+        end
+      end)
+    end
+  end
+
+  input_object :me_input do
+    field(:token, non_null(:string))
+  end
+
+  object :me_payload do
+    field(:id, non_null(:string))
+    field(:token, non_null(:string))
+    field(:user_id, non_null(:string))
+    field(:user, non_null(:user))
+    field(:inserted_at, non_null(:naive_datetime))
+  end
+
+  object :auth_query do
+    field :fizz, :string do
+      resolve(fn _, _ -> {:ok, "Fizz buzz"} end)
+    end
+
+    field :me, type: :me_payload do
+      arg(:auth, non_null(:me_input))
+
+      resolve(fn %{auth: %{token: token}}, _ ->
+        case Server.Auth.get_user_session_by_token(token) do
+          %Server.Auth.UserSession{} = user_session ->
+            {:ok, user_session |> Server.Repo.preload(:user)}
+
+          nil ->
+            {:error, "Session not found"}
         end
       end)
     end
